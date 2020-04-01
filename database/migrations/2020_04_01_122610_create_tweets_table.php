6d53615310a897c38054ad9eb767514e421234a5
Luan P. Coelho <luan@byteslivres.com.br>
2020-04-01 10:02:18 -0300

    Migration created

---
+++ database/migrations/2020_04_01_122610_create_tweets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('text', 280);
+            $table->string('username', 55);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tweets');
+    }
+}
---
+++ database/migrations/2020_04_01_122630_create_hashtags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHashtagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hashtags', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('hashtag', 55);
+
+            $table->bigInteger('tweets_id')->unsigned();
+            $table->foreign('tweets_id')->references('id')->on('tweets')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hashtags');
+    }
+}

82a1af7f075c6c37178937098ff804a6a956e488
Luan P. Coelho <luan@byteslivres.com.br>
2020-04-06 21:55:42 -0300

    Adjustment for run inital docker

---
+++ run_intitial.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+echo 'Run setup...'
+
+echo ' - Docker Build'
+docker-compose build
+
+echo ' - Docker Up'
+docker-compose up -d
+
+echo 'Waiting 15 seconds..'
+sleep 15
+
+echo ' - Copy .env'
+cp .env.example .env
+
+echo 'Run composer PHP'
+docker run --rm -v $(pwd):/app composer install
+
+echo ' - Artisan Migrate'
+docker exec php /bin/sh -c 'php artisan migrate'
+
+echo "Status:"
+docker-compose ps | grep "nginx" | cut -d ";" -f 2
+
+echo "End script"
